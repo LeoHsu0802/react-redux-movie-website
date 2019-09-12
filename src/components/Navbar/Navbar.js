454085ff17185c076756305653cd478d02e4f91f
Leo Hsu <38006639+LeoHsu0802@users.noreply.github.com>
2019-09-12 13:26:43 +0800

    add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,12 @@ import './Navbar.css'
 function navbar() {
     return (
         <div className="navbar">
-            <h1>Navbar</h1>
+            <div className="sidebar">></div>
+            <h1 className="webname">Movie System</h1>
+            <input placeholder="Search Movie..."/>
+            <a>User</a>
+            <a>Help</a>
+            <a>Home</a>
         </div>
     )
 }
