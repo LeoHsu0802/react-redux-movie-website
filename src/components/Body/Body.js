d16a9e062bc1dbc0d7e478ae8eb2f95a8ca97ee8
Leo Hsu <38006639+LeoHsu0802@users.noreply.github.com>
2019-09-12 12:49:18 +0800

    New

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+            <h1>Movie Pics Display</h1>
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+function navbar() {
+    return (
+        <div className="navbar">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default navbar
